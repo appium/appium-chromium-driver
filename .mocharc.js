837a20f93ade5a029646d10a0740c8b118b0e8e9
Jonathan Lipps <jlipps@gmail.com>
2022-12-13 11:21:13 -0800

    chore: scaffold out build system

---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+'use strict';
+
+module.exports = {
+  require: [require.resolve('./test/setup.js')],
+  // forbids use of .only() in CI
+  forbidOnly: Boolean(process.env.CI),
+  color: true,
+  // increase default timeout for CI since it can be slow
+  timeout: process.env.CI ? '5s' : '2s',
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const {ChromeDriver} = require('./build/lib/driver.js');
+module.exports = ChromeDriver;
