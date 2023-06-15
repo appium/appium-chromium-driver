43023e4b87fd84a7bb251230ccea6dde9cda2867
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-06-15 02:14:22 -0700

    needed chromedriver

--- test/e2e/driver.spec.js
+++ test/e2e/driver.spec.js
@@ -15,7 +15,7 @@ const DEF_CAPS = {
 
 // GitHub Actions
 if (process.env.CHROMEWEBDRIVER) {
-  DEF_CAPS['appium:executable'] = process.env.CHROMEWEBDRIVER;
+  DEF_CAPS['appium:executable'] = `${process.env.CHROMEWEBDRIVER}/chromedriver`;
 }
 
 if (CHROME_BIN) {
