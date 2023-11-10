c262131ad7f3113aac39dd78f731d8bd39b2fdad
Jonathan Lipps <jlipps@gmail.com>
2023-11-10 14:06:18 +0000

    feat: add webdriver bidi support

--- test/e2e/driver.spec.js
+++ test/e2e/driver.spec.js
@@ -2,18 +2,19 @@ import {main as startAppium} from 'appium';
 import path from 'path';
 import {remote} from 'webdriverio';
 
-
-
-const PLATFORM = process.env.TEST_PLATFORM || 'macOS';
+const PLATFORM = process.env.TEST_PLATFORM || 'mac';
 const PORT = process.env.TEST_PORT || 4780;
 const HOST = '127.0.0.1';
 const CHROME_BIN = process.env.TEST_CHROME;
+
+const SERVER_URL = `http://${HOST}:${PORT}`;
 
 const DEF_CAPS = {
   platformName: PLATFORM,
   browserName: 'chrome',
   'appium:automationName': 'Chromium',
   'appium:autodownloadEnabled': true,
+  webSocketUrl: true,
 };
 
 // GitHub Actions
@@ -37,6 +38,25 @@ const WDIO_OPTS = {
   capabilities: DEF_CAPS,
 };
 
+function setupDriver() {
+  /** @type {{driver: import('webdriverio').Browser}} */
+  let ctx = {driver: null};
+
+  before(async function() {
+    ctx.driver = await remote(WDIO_OPTS);
+  });
+
+  after(async function() {
+    if (ctx.driver) {
+      await ctx.driver.deleteSession();
+      ctx.driver = null;
+    }
+  });
+
+  return ctx;
+}
+
+
 describe('ChromeDriver', function() {
   /** @type import('@appium/types').AppiumServer */
   let appium;
@@ -50,26 +70,52 @@ describe('ChromeDriver', function() {
   });
 
   describe('basic session handling', function() {
-    /** @type import('webdriverio').Browser<'async'> */
-    let driver;
-
-    before(async function() {
-      driver = await remote(WDIO_OPTS);
-    });
-
-    after(async function() {
-      if (driver) {
-        await driver.deleteSession();
-        driver = null;
-      }
-    });
+    const ctx = setupDriver();
 
     it('should navigate to a url', async function() {
-      await driver.navigateTo(`http://${HOST}:${PORT}/status`);
+      await ctx.driver.navigateTo(`${SERVER_URL}/status`);
     });
 
     it('should get page soruce', async function() {
-      await driver.getPageSource().should.eventually.match(/value.+build.+version/);
+      await ctx.driver.getPageSource().should.eventually.match(/value.+build.+version/);
     });
+  });
+
+  describe('bidi commands', function() {
+    const ctx = setupDriver();
+
+    it('should navigate to a url', async function() {
+      const d = ctx.driver;
+      const {contexts} = await d.browsingContextGetTree({});
+      await d.browsingContextNavigate({
+        context: contexts[0].context,
+        url: `${SERVER_URL}/test/guinea-pig`,
+        wait: 'complete',
+      });
+      await d.getUrl().should.eventually.include('guinea-pig');
+    });
+
+    it('should execute javascript', async function() {
+      const d = ctx.driver;
+      const {contexts} = await d.browsingContextGetTree({});
+      const res = await d.scriptEvaluate({
+        expression: 'document.title',
+        target: {context: contexts[0].context},
+        awaitPromise: false,
+      });
+      res.result.value.should.eql('I am a page title');
+    });
+
+    it('should receive bidi events', async function() {
+      const d = ctx.driver;
+      const {contexts} = await d.browsingContextGetTree({});
+      const networkResponses = [];
+      d.on('network.responseCompleted', (response) => networkResponses.push(response));
+      await d.sessionSubscribe({events: ['network.responseCompleted'], contexts: [contexts[0].context]});
+      networkResponses.should.be.empty;
+      await d.navigateTo(`${SERVER_URL}/test/guinea-pig`);
+      networkResponses.should.not.be.empty;
+    });
+
   });
 });
