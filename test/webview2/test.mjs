ba4aca595ca732323ef9e999df37c929f16331be
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-05-01 08:48:40 -0700

    tweak

--- test/webview2/test.mjs
+++ test/webview2/test.mjs
@@ -1,20 +1,19 @@
 import path from 'node:path';
 import process from 'node:process';
-import Chromedriver from 'appium-chromedriver';
 
-const edgeDriverExe = path.join(process.env.EDGEWEBDRIVER, 'msedgedriver.exe');
 const appBinary = path.resolve(process.env.WEBVIEW2_EXE);
 
 async function main() {
   const appiumPkg = await import('appium');
   const appium = await appiumPkg.default.main({port: 4780});
 
-  const {remote} = await import('webdriverio');
-  const driver = await remote({
-    hostname: '127.0.0.1',
-    port: 4780,
-    connectionRetryCount: 0,
-    capabilities: {
+  try {
+    const {remote} = await import('webdriverio');
+    const driver = await remote({
+      hostname: '127.0.0.1',
+      port: 4780,
+      connectionRetryCount: 0,
+      capabilities: {
         platformName: 'windows',
         browserName: 'msedge',
         'appium:automationName': 'Chromium',
@@ -23,13 +22,19 @@ async function main() {
         'ms:edgeOptions': {
           binary: appBinary,
         },
-    },
+      },
     });
 
-  await driver.deleteSession();
+    await driver.deleteSession();
+  } finally {
+    await appium.close();
+  }
 }
 
-main().catch(async (err) => {
+try {
+  await main();
+} catch (err) {
+  // eslint-disable-next-line no-console
   console.error(err);
   process.exit(1);
-});
+}
